d7a24d0a932064677892ea8aed899dfb6d72c6ad
Jose Eduardo Hernandez Alvarado <joedhernandez95@unitec.edu>
2015-03-15 12:34:20 -0600

    agregando arbolb.h

---
+++ agregando arbolb.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "primaryKey.h"
+#include "nodo.h"
+#include <iostream>
+using namespace std;
+
+class ArbolB{
+private:
+	Nodo *raiz; //Puntero al nodo raiz
+	int grado; //grado minimo
+public:
+	//Constructor(Inicializa el arbol como vacio)
+	ArbolB(int degree);
+	//Funcion para recorrer el arbol
+	void recorrer();
+	//Funcion para buscar una llave en este arbol
+	Nodo* buscar(PrimaryKey*);
+};
