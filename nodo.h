940819a9aec88b5a9ee9769e7d95bc9a613123d6
Jose Eduardo Hernandez Alvarado <joedhernandez95@unitec.edu>
2015-03-15 12:33:19 -0600

    agregando nodo.h

---
+++ nodo.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include "primaryKey.h"
+using namespace std;
+
+//Clase Nodo del arbol B
+class Nodo{
+private:
+	PrimaryKey** llaves; //un arreglo de llaves
+	int grado; //el grado minimo del arbol
+	Nodo **hijos; //un arreglo de punteros a los hijos
+	int numLlaves; //Numero actual de llaves
+	bool isLeaf; //Es verdadero cuando el nodo actual es una hoja
+public:
+	Nodo(int degree,bool leaf); //Constructor
+	//Una funcion para recorrer los nodos en un subarbol que esta conectado a este nodo
+	void recorrer();
+	//Una funcion para buscar una llave en un subarbol conectado a este nodo 
+	Nodo *buscar(PrimaryKey*); //retorna NULL si la llave no esta presenta
+};
+
+
