1bfb02ba5adca6e5eff3436c3e8906f0ac3f4601
Jose Eduardo Hernandez Alvarado <joedhernandez95@unitec.edu>
2015-03-15 15:16:14 -0600

    update nodo.h

--- nodo.h
+++ nodo.h
@@ -6,9 +6,9 @@ using namespace std;
 //Clase Nodo del arbol B
 class Nodo{
 private:
-	PrimaryKey** llaves; //un arreglo de llaves
+	PrimaryKey** llaves; //un arreglo de punteros a las llaves
 	int grado; //el grado minimo del arbol
-	Nodo **hijos; //un arreglo de punteros a los hijos
+	Nodo** hijos; //un arreglo de punteros a los hijos
 	int numLlaves; //Numero actual de llaves
 	bool isLeaf; //Es verdadero cuando el nodo actual es una hoja
 public:
@@ -16,7 +16,9 @@ public:
 	//Una funcion para recorrer los nodos en un subarbol que esta conectado a este nodo
 	void recorrer();
 	//Una funcion para buscar una llave en un subarbol conectado a este nodo 
-	Nodo *buscar(PrimaryKey*); //retorna NULL si la llave no esta presenta
+	Nodo* buscar(PrimaryKey*); //retorna NULL si la llave no esta presenta
+	//friend class ArbolB;
 };
+
 
 
